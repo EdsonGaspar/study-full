bcd9cabf0733170a8e75485e0214fbebf3a35900
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-11-29 04:55:10 +0100

    Feat: Primeiro resultado da transpilacao com o Babel

---
+++ studyreact/lib/bundle.js/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _users$location;
+const users = {
+  nome: "Edson Gaspar"
+};
+let showUser = (_users$location = users.location) === null || _users$location === void 0 ? void 0 : _users$location.street;
+console.log(showUser);
