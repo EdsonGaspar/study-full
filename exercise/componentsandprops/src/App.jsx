923ca389cafc8b73f0668f675e972a8396de09b1
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-12-02 16:09:59 +0100

    Feat: Finalizado o primeiro desafio

--- exercise/componentsandprops/src/App.jsx
+++ exercise/componentsandprops/src/App.jsx
@@ -1,8 +1,10 @@
+import { Welcome } from "./components/welcome";
 import "./styles/global.scss";
 function App() {
   return (
     <main>
       <h1>Exercio sobre compontes e propriedades</h1>
+      <Welcome nome="Edson Joao Gaspar" />
     </main>
   );
 }
---
+++ exercise/componentsandprops/src/components/welcome.jsx
@@ -0,0 +1,7 @@
+export function Welcome(props) {
+  return (
+    <section>
+      <p>Seja bem vindo, {props.nome}</p>
+    </section>
+  );
+}
