c3756ce0a991c0a78c865b500e833de0205b7756
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-12-11 18:03:59 +0100

    Feat: Consolidando o estudo do UseEffect, e consumindo API com fecth.

--- studyreact/src/components/repositoryList.jsx
+++ studyreact/src/components/repositoryList.jsx
@@ -1,8 +1,20 @@
 import { Counter } from "./couter";
 import { RepositoryItems } from "./repositoryitems";
 import "../styles/stylelistrepository.scss";
+import { useEffect, useState } from "react";
+
+//https://api.github.com/users/EdsonGaspar/repos
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/EdsonGaspar/repos").then((response) =>
+      response.json().then((data) => setRepositories(data))
+    );
+  }, []);
+
+  console.log(repositories);
   return (
     <section className="list-repository">
       <h1>Lista de Repositorio</h1>
